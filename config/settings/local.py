9cc14f4d980c46270db22aa74888dd93db21ea31
terraone <jeon2457@gmail.com>
2022-11-16 23:40:23 +0900

    setting 변경

---
+++ config/settings/local.py
@@ -0,0 +1,4 @@
+from .base import *  # base.py 파일의 모든 내용을 사용한다는 의미
+
+# local.py 파일의 내용은 base.py 파일과 동일하지만 ALLOWED_HOSTS만 다르게 설정하겠다는 의미
+ALLOWED_HOSTS = []
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['3.37.58.70']  # 본인 아이피 주소를 입력한다.
